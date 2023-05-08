06e1f5c4e4dfe3a31d80fe24d6da050f287164f1
Mohamad Meerasa <mohamadmeerasa45@gmail.com>
2023-05-08 09:22:48 +0530

    Songs Updation

--- public/script.js
+++ public/script.js
@@ -19,7 +19,7 @@ const songs = ['1.VaaVaathi','2.Vaa Vaathi','3.Unn Per Solla','4.Mudhal Kanave',
         '26.Onna Pola Oruthana','27.Aattam Pottu','28.Adhuva Idhuva','29.Ovvundraai Thirudugiraai','30.Yembuttu Irukkuthu Aasai',
         '31.Per Vachaalum Vaikkaama','32.Arabic Kuthu','33.Private Party','34.Dippam Dappam','35.Thee Thalapathy','36.Jimmiki Ponnu','37.Celebration Of Varisu','38.July Malargale','39.Ootha Ootha',
         '40.Vizhi Moodi Yosithaal','41.Vaarayo Vaarayo','42.Hasili Fisiliye','43.Amali Thumali','44.Ennamo Yeadho','45.Yaar Indha',
-        '46.Enakkoru Sinegidhi','47.Ennavo Ennavo','48.June July Maathadhil','49.Mellinamae Mellinamae','50.Minnalai Pidithu','51.Sakkarai Nilave',
+        '46.Enakkoru Sinegidhi','47.Ennavo Ennavo','48.June July Maathadhil','49.Mellinamae Mellinamae','50.Minnalai Pidithu','51.Sakkarai Nilave','52.Aluva Puzha','53.Malare Ninne',
 ];// Keep track of song
 let songIndex = 0;
 
